3ff8f5562298cbde44207ff4dd4fb418aa9a69cb
Luciana Cuarelli <luciananightwish@gmail.com>
2013-10-26 17:11:41 -0200

    Alerta para verificar lista retornada

--- js/employeelist.js
+++ js/employeelist.js
@@ -8,15 +8,16 @@ $('#employeeListPage').bind('pageinit', function(event) {
 
 function getEmployeeList() {
 	$.getJSON(serviceURL, function(data) {
-		$('#employeeList li').remove();
-		employees = data.items;
-		$.each(employees, function(index, employee) {
-			$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
-					/*'<img src="pics/' + employee.picture + '"/>' +*/
-					'<h4>' + employee.title + '</h4>' +
-					'<p>' + employee.description + '</p>' +
-					'<span class="ui-li-count">' + "Brasil" + '</span></a></li>');
-		});
-		$('#employeeList').listview('refresh');
+		alert(data);
+		//$('#employeeList li').remove();
+		//employees = data.items;
+		//$.each(employees, function(index, employee) {
+		//	$('#employeeList').append('<li><a href="employeedetails.html?id=' + employee.id + '">' +
+		//			/*'<img src="pics/' + employee.picture + '"/>' +*/
+		//			'<h4>' + employee.title + '</h4>' +
+		//			'<p>' + employee.description + '</p>' +
+		//			'<span class="ui-li-count">' + "Brasil" + '</span></a></li>');
+		//});
+		//$('#employeeList').listview('refresh');
 	});
 }
