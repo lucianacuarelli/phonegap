954d088fd15bfdb8f62e4d3fed057b84c4a5b1c5
Luciana Cuarelli <luciananightwish@gmail.com>
2013-10-31 23:10:49 -0200

    Javascript

--- js/employeedetails.js
+++ js/employeedetails.js
@@ -11,6 +11,8 @@ function displayEmployee(data) {
 	
 	//Obtem lista de palavras retornadas do JSON para o exercício selecionado
 	var information = data[0].information;
+	//Coloca tudo em letra maiuscula
+	information = information.toUpperCase();
 	//Obtem palavras individuais
 	var arWords = information.split(",");
 	//Randomiza a seleção de palavras
@@ -26,29 +28,6 @@ function displayEmployee(data) {
 	alert(exercexerise.description);
 	$('#city').text(exercise.exer);
 	alert(exercise.exer);*/
-
-	if (employee.managerId>0) {
-		$('#actionList').append('<li><a href="employeedetails.html?id=' + employee.managerId + '"><h3>View Manager</h3>' +
-				'<p>' + employee.managerFirstName + ' ' + employee.managerLastName + '</p></a></li>');
-	}
-	if (employee.reportCount>0) {
-		$('#actionList').append('<li><a href="reportlist.html?id=' + employee.id + '"><h3>View Direct Reports</h3>' +
-				'<p>' + employee.reportCount + '</p></a></li>');
-	}
-	if (employee.email) {
-		$('#actionList').append('<li><a href="mailto:' + employee.email + '"><h3>Email</h3>' +
-				'<p>' + employee.email + '</p></a></li>');
-	}
-	if (employee.officePhone) {
-		$('#actionList').append('<li><a href="tel:' + employee.officePhone + '"><h3>Call Office</h3>' +
-				'<p>' + employee.officePhone + '</p></a></li>');
-	}
-	if (employee.cellPhone) {
-		$('#actionList').append('<li><a href="tel:' + employee.cellPhone + '"><h3>Call Cell</h3>' +
-				'<p>' + employee.cellPhone + '</p></a></li>');
-		$('#actionList').append('<li><a href="sms:' + employee.cellPhone + '"><h3>SMS</h3>' +
-				'<p>' + employee.cellPhone + '</p></a></li>');
-	}
 	$('#actionList').listview('refresh');
 	
 }
@@ -63,4 +42,87 @@ function getUrlVars() {
         vars[hash[0]] = hash[1];
     }
     return vars;
+}
+
+function Forca(params){
+
+	var root = this.elemento = params.Elemento || document.body;
+	this.Palavras = params.Palavras || []; // vetor de palavras
+	this.Palavra =" ";  // palavra corrente
+	this.Erros = 0; // número de errros
+	this.Boneco = " "; // desenho do boneco
+	
+	this.Teclado = function(){ // metodo responsável por montar o teclado.
+	var teclado = document.getElementById("forca_teclado");
+	if(!teclado){
+	teclado = document.createElement("div");
+	teclado.setAttribute("id","forca_teclado");
+	teclado.setAttribute("class","teclado");
+	for(var i = 65; i < 91; i++){
+	var tecla = document.createElement("div");
+	tecla.setAttribute("class","tecla");
+	tecla.innerHTML = String.fromCharCode(i);
+	tecla.setAttribute("title",String.fromCharCode(i));
+	if(tecla.addEventListener )
+	tecla.addEventListener(‘click’,function(){ root.Validar(this); },false);
+	else
+	tecla.attachEvent(‘onclick’,function(e){ root.Validar(event.srcElement);  });
+	teclado.appendChild(tecla);
+	}
+	elemento.appendChild(teclado);
+	}
+	}
+	this.Sorteio = function(){ //Método responsável pelo sorteio de uma nova palavra.
+	var novoIndice = Math.ceil(Math.random()*params.Palavras.length-1)
+	root.Palavra = params.Palavras[novoIndice];
+	var painel = document.getElementById("forca_painel");
+	if(!painel){
+	painel = document.createElement("div");
+	painel.setAttribute("id","forca_painel");
+	painel.setAttribute("class","painel");
+	elemento.appendChild(painel)
+	}
+	painel.innerHTML = "";
+	for(var i = 0; i < root.Palavra.length;i++){
+	var letra = document.createElement("div");
+	letra.setAttribute("id",root.Palavra.substr(i,1));
+	letra.setAttribute("class","letra");
+	letra.innerHTML = "";//root.Palavra.substr(i,1);
+	painel.appendChild(letra);
+	}
+	}
+	this.Validar = function(letra){   // Método responsável por validar e desenhar o boneco.
+	var desenho = document.getElementById("forca_desenho");
+	if(!desenho){
+	desenho = document.createElement("div");
+	desenho.setAttribute("id","forca_desenho");
+	elemento.appendChild(desenho);
+	}
+	var boneco;
+	if(root.Palavra.indexOf(letra.title) == -1){
+	this.Erros++;
+	switch(this.Erros){
+	case 1: root.Boneco += "\n   o"; break;
+	case 2: root.Boneco += "\n  \/"; break;
+	case 3: root.Boneco += "|"; break;
+	case 4: root.Boneco += "\\"; break;
+	case 5: root.Boneco += "\n  /"; break;
+	case 6: root.Boneco += " \\\n <button onclick=’window.location.reload(true);’ style=’padding:0px;width:85px;border:1px solid black;’>GAME OVER</button>"; break;
+	}
+	desenho.innerHTML = " <pre> "+root.Boneco+" </pre> ";
+	}
+	else
+	{
+	var indices = [];
+	for(var i = 0; i < root.Palavra.length;i++)
+	if(root.Palavra.substr(i,1) == letra.title) indices.push(i);
+	painel = document.getElementById("forca_painel").getElementsByTagName("div");
+	for(var i = 0; i < indices.length; i++)
+	painel[indices[i]].innerHTML =  letra.title;
+	}
+	}
+	this.Comecar = function(){ // Método responsável por iniciar tudo, criado só para deixar a chamada mais organizada!  ;-) 
+	root.Teclado();
+	root.Sorteio();
+	}
 }
