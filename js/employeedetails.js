51de40ef52edbb63f78c34e49774522bc4eab4ca
Luciana Cuarelli <luciananightwish@gmail.com>
2013-11-01 00:34:34 -0200

    Javascript - Forca

--- js/employeedetails.js
+++ js/employeedetails.js
@@ -3,79 +3,7 @@ $('#detailsPage').live('pageshow', function(event) {
 	$.getJSON(serviceURL + '?id='+id, displayEmployee);
 });
 
-$(function() {
-
-	var word;
-	var remainingLetters;
-	var secret;
-	var currentImage = 1;
-	
-	function setup(theWord) {
-		word = theWord.toUpperCase();
-		remainingLetters = word;
-		secret = word.replace(/./g, '_');
-		$('#secret').text(secret);
-	}
-	
-	var words=['CAT','CAR','BAG','BAR','CREEP','ANDROID']
-	
-	//Obtem lista de palavras retornadas do JSON para o exercício selecionado
-	var information = data[0].information;
-	//Coloca tudo em letra maiuscula
-	information = information.toUpperCase();
-	//Obtem palavras individuais
-	var arWords = information.split(",");
-	//Randomiza a seleção de palavras
-	var theWord = arWords[Math.floor((Math.random()*arWords.length)+1)];
-
-	setup(theWord);
-					
-	String.prototype.replaceAt = function(index, character) {
-		return this.substr(0, index) + character + this.substr(index + character.length);
-	}
-	
- 	function processLetter(letter) {
-		var found = false;
-		// alert(remainingLetters);
-		for (var i = 0 ; i < remainingLetters.length ; i++) {
-			//alert(remainingLetters.charAt(i));
-			if (remainingLetters.charAt(i) == letter) {
-				remainingLetters = remainingLetters.replaceAt(i, '_');
-				secret = secret.replaceAt(i, letter);
-				found = true;	
-			}		
-		 }      
-		if (found) {
-			$('#secret').text(secret);
-			if (secret.indexOf('_') == -1) {
-				alert('Você ganhou!');
-				location.reload();
-			}else {
-				currentImage++;
-				var imageId = "#hangman_" + currentImage;
-				$(imageId).fadeTo(300, 1.0, function() {
-					if (currentImage == 7) {
-						$('#secret').text(word);
-						alert('Você perdeu!');
-						location.reload();
-	
-					}
-				});
-			}
-		}
-	}
-	
-	$('.letters span').click(function(event){
-		var letter = event.target.innerText;
-		alert(letter);
-		$(event.target).unbind("click");
-		$(event.target).addClass("disabled");
-		processLetter(letter);
-	});
-});
-
-
-/*var word;
+var word;
 var remainingLetters;
 var secret;
 
@@ -111,9 +39,46 @@ function displayEmployee(data) {
 		processLetter(letter);
 	});
 	
+	/*$('#employeePic').attr('src', 'pics/james_king.png');
+	alert(exercise.title);
+	
+	alert(exercexerise.description);
+	$('#city').text(exercise.exer);
+	alert(exercise.exer);*/
 	$('#actionList').listview('refresh');
 	
 }
+
+String.prototype.replaceAt = function(index, character) {
+		return this.substr(0, index) + character + this.substr(index + character.length);
+}
+
+function processLetter(letter) {
+	var found = false;
+	// alert(remainingLetters);
+	for (var i = 0 ; i < remainingLetters.length ; i++) {
+		//alert(remainingLetters.charAt(i));
+		if (remainingLetters.charAt(i) == letter) {
+			remainingLetters = remainingLetters.replaceAt(i, '_');
+			secret = secret.replaceAt(i, letter);
+			found = true;	
+		}		
+	 }      
+	if (found) {
+		$('#secret').text(secret);
+		if (secret.indexOf('_') == -1) {
+			alert('Você ganhou!');
+		}else {
+			currentImage++;
+			if (currentImage == 7) {
+				$('#secret').text(word);
+				alert('Você perdeu!');
+
+			}
+		}
+	}
+}
+
 
 function getUrlVars() {
     var vars = [], hash;
@@ -126,38 +91,3 @@ function getUrlVars() {
     }
     return vars;
 }
-
-function replaceAt(str,index,chr) {
-    if(index > str.length-1) return str;
-    return str.substr(0,index) + chr + str.substr(index+1);
-}
-
-function processLetter(letter) {
-	var found = false;
-	for (var i = 0 ; i < remainingLetters.length ; i++) {
-		if (remainingLetters.charAt(i) == letter) {
-			remainingLetters = remainingLetters.replaceAt(i, '_');
-			secret = replaceAt(secret, i, letter);
-			$('#secret').text(secret);
-			found = true;	
-		}		
-	}      
-	if (found) {
-		$('#secret').text(secret);
-		if (secret.indexOf('_') == -1) {
-			alert('Você ganhou!');
-			location.reload();
-		}else {
-			currentImage++;
-			var imageId = "#hangman_" + currentImage;
-			$(imageId).fadeTo(300, 1.0, function() {
-				if (currentImage == 7) {
-					$('#secret').text(word);
-					alert('Você perdeu!');
-					location.reload();
-
-				};
-			});
-		}
-	}
-}*/
