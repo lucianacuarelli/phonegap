87c787762ae91558b4956fdeb438b3770ae47c0d
Luciana Cuarelli <luciananightwish@gmail.com>
2013-11-01 00:27:45 -0200

    Javascript - Forca

--- js/employeedetails.js
+++ js/employeedetails.js
@@ -3,7 +3,81 @@ $('#detailsPage').live('pageshow', function(event) {
 	$.getJSON(serviceURL + '?id='+id, displayEmployee);
 });
 
-var word;
+$(function() {
+
+	var word;
+	var remainingLetters;
+	var secret;
+	var currentImage = 1;
+	
+	function setup(theWord) {
+		word = theWord.toUpperCase();
+		remainingLetters = word;
+		secret = word.replace(/./g, '_');
+		$('#secret').text(secret);
+	}
+	
+	var words=['CAT','CAR','BAG','BAR','CREEP','ANDROID']
+	
+	var theWord = prompt("Please enter your word(for 2 player), or just hit return for  a random word [for single player]" );
+	
+	//Obtem lista de palavras retornadas do JSON para o exercício selecionado
+	var information = data[0].information;
+	//Coloca tudo em letra maiuscula
+	information = information.toUpperCase();
+	//Obtem palavras individuais
+	var arWords = information.split(",");
+	//Randomiza a seleção de palavras
+	var theWord = arWords[Math.floor((Math.random()*arWords.length)+1)];
+
+	setup(theWord);
+					
+	String.prototype.replaceAt = function(index, character) {
+		return this.substr(0, index) + character + this.substr(index + character.length);
+	}
+	
+ 	function processLetter(letter) {
+		var found = false;
+		// alert(remainingLetters);
+		for (var i = 0 ; i < remainingLetters.length ; i++) {
+			//alert(remainingLetters.charAt(i));
+			if (remainingLetters.charAt(i) == letter) {
+				remainingLetters = remainingLetters.replaceAt(i, '_');
+				secret = secret.replaceAt(i, letter);
+				found = true;	
+			}		
+		 }      
+		if (found) {
+			$('#secret').text(secret);
+			if (secret.indexOf('_') == -1) {
+				alert('Você ganhou!');
+				location.reload();
+			}else {
+				currentImage++;
+				var imageId = "#hangman_" + currentImage;
+				$(imageId).fadeTo(300, 1.0, function() {
+					if (currentImage == 7) {
+						$('#secret').text(word);
+						alert('Você perdeu!');
+						location.reload();
+	
+					}
+				});
+			}
+		}
+	}
+	
+	$('.letters span').click(function(event){
+		var letter = event.target.innerText;
+		alert(letter);
+		$(event.target).unbind("click");
+		$(event.target).addClass("disabled");
+		processLetter(letter);
+	});
+});
+
+
+/*var word;
 var remainingLetters;
 var secret;
 
@@ -39,12 +113,6 @@ function displayEmployee(data) {
 		processLetter(letter);
 	});
 	
-	/*$('#employeePic').attr('src', 'pics/james_king.png');
-	alert(exercise.title);
-	
-	alert(exercexerise.description);
-	$('#city').text(exercise.exer);
-	alert(exercise.exer);*/
 	$('#actionList').listview('refresh');
 	
 }
@@ -94,4 +162,4 @@ function processLetter(letter) {
 			});
 		}
 	}
-}
+}*/
